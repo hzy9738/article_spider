c9a2ec177123f22b7e2f239747fa1a319be91085
hzy9738 <hzy9738@126.com>
2018-09-29 19:08:23 +0800

    bole hah

--- article_spider/spiders/jobbole.py
+++ article_spider/spiders/jobbole.py
@@ -1,11 +1,40 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
+from scrapy.http import Request
+from urllib import parse
 
 
 class JobboleSpider(scrapy.Spider):
-    name = 'jobbole'
+    name = "jobbole"
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
-        pass
+
+        post_urls = response.css('#archive .floated-thumb .post-meta a.archive-title::attr(href)').extract()
+        for post_url in post_urls:
+            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse_detail)
+
+        next_url = response.css('.next.page-numbers::attr("href")').extract_first('')
+        if next_url:
+            yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
+
+    def parse_detail(self, response):
+        # 提取文章的具体字段
+        title = response.xpath('//*[@id="post-110287"]/div[1]/h1/text()').extract_first('')
+        create_time = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract_first(
+            '').strip().replace(
+            "·", " ").strip()
+        praise_nums = response.xpath('//span[contains(@class,"vote-post-up")]//h10/text()').extract_first('')
+        fav_nums = response.xpath('//span[contains(@class,"bookmark-btn")]/text()').extract_first('')
+        fav_match = re.match(".*?(\d+)", fav_nums)
+        if fav_match:
+            fav_nums = fav_match.group(1)
+        comments_nums = response.xpath('//a[@href="#article-comment"]/span/text()').extract_first('')
+        comments_match = re.match(".*?(\d+)", comments_nums)
+        if comments_match:
+            comments_nums = comments_match.group(1)
+        tag_list = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/a/text()').extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith('评论')]
+        tags = ",".join(tag_list)
