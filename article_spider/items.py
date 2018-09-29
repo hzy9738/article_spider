7be2ce07cf991c6058e0f9a9859d14df682860cf
hzy9738 <hzy9738@126.com>
2018-09-30 00:29:02 +0800

    md5-url

--- article_spider/items.py
+++ article_spider/items.py
@@ -13,6 +13,15 @@ class ArticleSpiderItem(scrapy.Item):
     # name = scrapy.Field()
     pass
 
-class ArticleItem(scrapy.Item):
+class JobBoleArticleItem(scrapy.Item):
     title = scrapy.Field()
-    # create_date = scrapy.
+    create_date = scrapy.Field()
+    url = scrapy.Field()
+    url_object_id = scrapy.Field()
+    front_image_url = scrapy.Field()
+    fornt_image_path = scrapy.Field()
+    praise_nums = scrapy.Field()
+    comment_nums = scrapy.Field()
+    fav_nums = scrapy.Field()
+    tags = scrapy.Field()
+    content = scrapy.Field()
--- article_spider/spiders/jobbole.py
+++ article_spider/spiders/jobbole.py
@@ -3,7 +3,8 @@ import scrapy
 import re
 from scrapy.http import Request
 from urllib import parse
-
+from article_spider.items import JobBoleArticleItem
+from article_spider.utils.common import get_md5
 
 class JobboleSpider(scrapy.Spider):
     name = "jobbole"
@@ -16,14 +17,20 @@ class JobboleSpider(scrapy.Spider):
         for post_node in post_nodes:
             image_url = post_node.css('img::attr(src)').extract_first("")
             post_url = post_node.css("::attr(href)").extract_first("")
-            yield Request(url=parse.urljoin(response.url, post_url),meta={"front_image_url": image_url}, callback=self.parse_detail)
+            yield Request(url=parse.urljoin(response.url, post_url), meta={"front_image_url": image_url},
+                          callback=self.parse_detail)
 
-        next_url = response.css('.next.page-numbers::attr("href")').extract_first('')
-        if next_url:
-            yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
+        # next_url = response.css('.next.page-numbers::attr("href")').extract_first('')
+        # if next_url:
+        #     yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
 
     def parse_detail(self, response):
+
+        article_item = JobBoleArticleItem()
+
         # 提取文章的具体字段
+
+        front_image_url = response.meta.get("front_image_url", "")
         title = response.xpath('//*[@id="post-110287"]/div[1]/h1/text()').extract_first('')
         create_time = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract_first(
             '').strip().replace(
@@ -40,3 +47,18 @@ class JobboleSpider(scrapy.Spider):
         tag_list = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/a/text()').extract()
         tag_list = [element for element in tag_list if not element.strip().endswith('评论')]
         tags = ",".join(tag_list)
+        content = response.xpath('//div[@class="entry"]').extract_first("")
+
+
+        article_item['title'] = title
+        article_item['url'] = response.url
+        article_item['url_object_id'] = get_md5(response.url)
+        article_item['create_date'] = create_time
+        article_item['front_image_url'] = [front_image_url]
+        article_item['praise_nums'] = praise_nums
+        article_item['comment_nums'] = comments_nums
+        article_item['fav_nums'] = fav_nums
+        article_item['tags'] = tags
+        article_item['content'] = content
+
+        yield article_item
